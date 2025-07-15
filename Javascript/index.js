aada853ace99385e5499fc2dba1f1fcc4e02f7af
Aurelia <m_aurelia@hotmail.fr>
2025-07-15 11:09:56 +0100

    improved select city function and added a link to go back to main page

--- Javascript/index.js
+++ Javascript/index.js
@@ -31,13 +31,14 @@ function updateCity(event) {
   citiesElement.innerHTML = `
     <div class="city">
       <div>
-        <h2>${cityTimeZone}</h2>
+        <h2>${cityName}</h2>
         <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
       </div>
       <div class="time">
         ${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small>
       </div>
     </div>
+    <a href="index.html">all cities</a>
   `;
 }
 updateTime();
